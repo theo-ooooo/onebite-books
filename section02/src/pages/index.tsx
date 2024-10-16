ecf3241dc8a3b0b5b2bf6712b97af36e488aaaa5
Theo <73323028+kkwondev@users.noreply.github.com>
2024-10-16 05:56:19 +0000

    ~2.6

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,3 +1,9 @@
+import style from "./index.module.css";
 export default function Home() {
-  return <h1>Index</h1>;
+  return (
+    <>
+      <h1 className={style.h1}>Index</h1>
+      <h2 className={style.h2}>H2</h2>
+    </>
+  );
 }
