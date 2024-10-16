2ed914bf49145158e1b8c45309359b43c7ef47da
Theo <73323028+kkwondev@users.noreply.github.com>
2024-10-16 05:36:54 +0000

    ~ 2-3

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,10 +1,32 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const onClickButton = () => {
+    router.push("/test");
+  };
+
+  useEffect(() => {
+    router.prefetch("/test");
+  }, []);
   return (
     <>
-      <header>global header</header>
+      <header>
+        <Link href='/'>index</Link>
+        &nbsp;
+        <Link href='/search' prefetch={false}>
+          search
+        </Link>
+        &nbsp;
+        <Link href='/book/1'>book/1</Link>
+        <div>
+          <button onClick={onClickButton}>/test로 페이지 이동</button>
+        </div>
+      </header>
       <Component {...pageProps} />
     </>
   );
