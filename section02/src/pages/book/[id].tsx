af5e158f4c8bee0d598bf8f58f2068fa04b32054
Theo <73323028+kkwondev@users.noreply.github.com>
2024-10-17 06:29:42 +0000

    ~ 2-13

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -1,4 +1,6 @@
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import style from "./[id].module.css";
+import fetchOneBook from "@/lib/fetch-one-book";
 
 const mockData = {
   id: 1,
@@ -12,9 +14,25 @@ const mockData = {
     "https://shopping-phinf.pstatic.net/main_3888828/38888282618.20230913071643.jpg",
 };
 
-export default function Page() {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const bookId = context.params!.id;
+
+  const book = await fetchOneBook(Number(bookId));
+  return {
+    props: { book },
+  };
+};
+
+export default function Page({
+  book,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  if (!book) {
+    return <div>문제가 발생했습니다.</div>;
+  }
   const { id, title, subTitle, description, author, publisher, coverImgUrl } =
-    mockData;
+    book;
   return (
     <div className={style.container}>
       <div
