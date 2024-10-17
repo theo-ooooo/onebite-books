94ce377a25d77107dbba1287b57926dc71470fda
Theo <73323028+kkwondev@users.noreply.github.com>
2024-10-17 02:37:10 +0000

    ~2.9

--- section02/src/pages/book/[[...id]].tsx
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function Page() {
-  const router = useRouter();
-  const { id } = router.query;
-  console.log(router);
-  return <h1>Book {id}</h1>;
-}
