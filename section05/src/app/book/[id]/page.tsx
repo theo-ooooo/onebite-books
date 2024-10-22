7c7b41cb2779698499f6eeb883c82c5c9d08e88b
Theo <73323028+kkwondev@users.noreply.github.com>
2024-10-22 08:28:04 +0000

    ~5.2

---
+++ section05/src/app/book/[id]/page.tsx
@@ -0,0 +1,37 @@
+import style from "./page.module.css";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string | string[] }>;
+}) {
+  const { id: paramsId } = await params;
+  const reseponse = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${paramsId}`,
+    { next: { revalidate: 10 } }
+  );
+
+  if (!reseponse.ok) {
+    return <div>오류가 발생하였습니다.</div>;
+  }
+
+  const book = await reseponse.json();
+  const { title, subTitle, description, author, publisher, coverImgUrl } = book;
+
+  return (
+    <div className={style.container}>
+      <div
+        className={style.cover_img_container}
+        style={{ backgroundImage: `url('${coverImgUrl}')` }}
+      >
+        <img src={coverImgUrl} />
+      </div>
+      <div className={style.title}>{title}</div>
+      <div className={style.subTitle}>{subTitle}</div>
+      <div className={style.author}>
+        {author} | {publisher}
+      </div>
+      <div className={style.description}>{description}</div>
+    </div>
+  );
+}
