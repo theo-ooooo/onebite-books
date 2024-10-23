8d3db4c2399b116e297b93a4c5f51e7920e672e9
Theo <73323028+kkwondev@users.noreply.github.com>
2024-10-23 07:11:06 +0000

    ~ 6.5

---
+++ section06/src/app/(with-searchbar)/search/error.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { startTransition, useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  const router = useRouter();
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+  return (
+    <>
+      <h2>검색 과정에서 에러가 발생하였습니다. </h2>
+      <button
+        onClick={() => {
+          startTransition(() => {
+            router.refresh();
+            reset();
+          });
+        }}
+      >
+        다시 시도
+      </button>
+    </>
+  );
+}
