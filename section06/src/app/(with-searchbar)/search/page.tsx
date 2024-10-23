a1c344cbe8472d6d799609e19ccefb255c12df0c
Theo <73323028+kkwondev@users.noreply.github.com>
2024-10-23 04:00:24 +0000

    ~ 6.2

---
+++ section06/src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,31 @@
+import BookItem from "@/components/book-item";
+import { BookData } from "@/types";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{
+    q?: string;
+  }>;
+}) {
+  const { q } = await searchParams;
+  const reseponse = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
+    {
+      next: { revalidate: 10 },
+    }
+  );
+
+  if (!reseponse.ok) {
+    return <div>오류가 발생하였습니다.</div>;
+  }
+
+  const books: BookData[] = await reseponse.json();
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
---
+++ section06/src/app/book/[id]/page.tsx
@@ -0,0 +1,43 @@
+import { notFound } from "next/navigation";
+import style from "./page.module.css";
+
+export const dynamic = "force-static";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string | string[] }>;
+}) {
+  const { id: paramsId } = await params;
+  const reseponse = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${paramsId}`,
+    { next: { revalidate: 10 } }
+  );
+
+  if (!reseponse.ok) {
+    if (reseponse.status === 404) {
+      notFound();
+    }
+    return <div>오류가 발생하였습니다.</div>;
+  }
+
+  const book = await reseponse.json();
+  const { title, subTitle, description, author, publisher, coverImgUrl } = book;
+
+  return (
+    <div className={style.container}>
+      <div
+        className={style.cover_img_container}
+        style={{ backgroundImage: `url('${coverImgUrl}')` }}
+      >
+        <img src={coverImgUrl} />
+      </div>
+      <div className={style.title}>{title}</div>
+      <div className={style.subTitle}>{subTitle}</div>
+      <div className={style.author}>
+        {author} | {publisher}
+      </div>
+      <div className={style.description}>{description}</div>
+    </div>
+  );
+}
