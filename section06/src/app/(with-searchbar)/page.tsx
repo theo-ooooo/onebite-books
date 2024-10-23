c998621b03a58f3be4e2964e763b748515b3bb45
Theo <73323028+kkwondev@users.noreply.github.com>
2024-10-23 05:25:46 +0000

    ~ 6.3

--- section06/src/app/(with-searchbar)/page.tsx
+++ section06/src/app/(with-searchbar)/page.tsx
@@ -1,8 +1,11 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import { BookData } from "@/types";
+import delay from "@/utils/delay";
+import { Suspense } from "react";
 
 async function AllBooks() {
+  await delay(1500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
     { cache: "force-cache" }
@@ -23,6 +26,7 @@ async function AllBooks() {
 }
 
 async function RecoBooks() {
+  await delay(3000);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
     { next: { revalidate: 3 } }
@@ -42,16 +46,22 @@ async function RecoBooks() {
   );
 }
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <RecoBooks />
+        <Suspense fallback={<div>Loading...111</div>}>
+          <RecoBooks />
+        </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <AllBooks />
+        <Suspense fallback={<div>Loading...222</div>}>
+          <AllBooks />
+        </Suspense>
       </section>
     </div>
   );
--- section06/src/app/(with-searchbar)/search/page.tsx
+++ section06/src/app/(with-searchbar)/search/page.tsx
@@ -1,14 +1,10 @@
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
+import delay from "@/utils/delay";
+import { Suspense } from "react";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{
-    q?: string;
-  }>;
-}) {
-  const { q } = await searchParams;
+async function SearchResult({ q }: { q: string }) {
+  await delay(1500);
   const reseponse = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
     {
@@ -27,5 +23,21 @@ export default async function Page({
         <BookItem key={book.id} {...book} />
       ))}
     </div>
+  );
+}
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{
+    q?: string;
+  }>;
+}) {
+  const { q } = await searchParams;
+
+  return (
+    <Suspense key={q || ""} fallback={<div>Loading...</div>}>
+      <SearchResult q={q || ""} />
+    </Suspense>
   );
 }
