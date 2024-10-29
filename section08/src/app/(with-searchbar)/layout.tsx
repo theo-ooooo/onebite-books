601dc5b6c7ffa579ba5153bdd3dd068ffddab642
Theo <73323028+kkwondev@users.noreply.github.com>
2024-10-29 06:33:37 +0000

    ~ 8.3

--- section08/src/app/(with-searchbar)/layout.tsx
+++ section08/src/app/(with-searchbar)/layout.tsx
@@ -1,7 +1,12 @@
 import { ReactNode, Suspense } from "react";
 import Searchbar from "../../components/searchbar";
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default function Layout({
+  children,
+}: {
+  children: ReactNode;
+  modal: ReactNode;
+}) {
   return (
     <div>
       <Suspense fallback={<div>loading...</div>}>
